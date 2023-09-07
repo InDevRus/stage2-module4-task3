01765a9f2a12c49a5d20b16c4555a83b23ce28dd
indev <InDevRus@hotmail.com>
2023-09-07 14:27:13 +0000

    Implemented FilteringStrategies

--- task1-strategy-pattern/src/main/java/com/mjc/stage2/impl/ManufactureFilteringStrategy.java
+++ task1-strategy-pattern/src/main/java/com/mjc/stage2/impl/ManufactureFilteringStrategy.java
@@ -1,11 +1,18 @@
 package com.mjc.stage2.impl;
 
 
+import com.mjc.stage2.FilteringStrategy;
+import com.mjc.stage2.Product;
 import lombok.AllArgsConstructor;
+import lombok.NonNull;
 
 @AllArgsConstructor
-public class ManufactureFilteringStrategy {
+public class ManufactureFilteringStrategy implements FilteringStrategy {
+    @NonNull
     private String manufacture;
 
-    // Write your code here!
+    @Override
+    public boolean filter(Product product) {
+        return String.CASE_INSENSITIVE_ORDER.compare(product.getManufacture(), manufacture) == 0;
+    }
 }
